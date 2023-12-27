3463eced2d7d3915294343515b855096290e89f2
Laimonukas <Laimisjakas@gmail.com>
2023-12-27 20:13:18 +0200

    initial upload of Butler scripts
    Automated build pushing to itch.io

---
+++ butlerPush.ps1
@@ -0,0 +1,9 @@
+butler push Pong/Exported/Exported.zip Laimonukas/pn:html
+butler push Shaders/Exported/Exported.zip Laimonukas/shaders:html
+butler push CrashCourse/Exported/Exported.zip Laimonukas/godot-crashcourse:html
+
+butler status Laimonukas/godot-crashcourse
+butler status Laimonukas/pn
+butler status Laimonukas/shaders
+
+Read-Host -Prompt "Press Enter to exit"
---
+++ butlerStatus.ps1
@@ -0,0 +1,6 @@
+
+butler status Laimonukas/godot-crashcourse
+butler status Laimonukas/pn
+butler status Laimonukas/shaders
+
+Read-Host -Prompt "Press Enter to exit"
