b4ecca26e3b28db6fbd3103942fc22f60fbb6907
Laimonukas <Laimisjakas@gmail.com>
2024-01-09 00:28:36 +0200

    JetRaidUpdate
    Updates to water shader - voronoi noise

--- JetRaid/Scripts/Jet.gd
+++ JetRaid/Scripts/Jet.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
 @export var moveSpeed = 10
-@onready var animSprite = get_node("AnimatedSprite2D")
-@onready var animPlayer = get_node("AnimationPlayer")
+@export var animSprite : AnimatedSprite2D
+@export var shadowSprite :AnimatedSprite2D
+@export var animPlayer : AnimationPlayer
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	animPlayer.play("idle")
 
@@ -18,6 +18,11 @@ func _process(delta):
 		position.x += moveSpeed
 		animSprite.flip_h = false
 		
-		
-	
+	SetShadow()
 	move_and_slide()
+
+
+func SetShadow():
+	shadowSprite.flip_h = animSprite.flip_h
+	shadowSprite.animation = animSprite.animation
+	shadowSprite.frame = animSprite.frame
