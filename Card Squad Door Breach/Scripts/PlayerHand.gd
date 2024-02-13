f9688714cccb6f23e4a0653524745f1bbb63a5ea
Laimonukas <Laimisjakas@gmail.com>
2024-02-14 00:00:37 +0200

    Update to camera controls

--- Card Squad Door Breach/Scripts/PlayerHand.gd
+++ Card Squad Door Breach/Scripts/PlayerHand.gd
@@ -2,6 +2,14 @@ extends Node2D
 class_name PlayerHand
 
 @export var handSlots : Array[Node2D]
+
+@export var maxCameraZoom : float = 5
+@export var minCameraZoom : float = 0.5
+@export var zoomSpeed : float = 0.2
+@export var dragSensitivity : float = 1.0
+@export var camNode : Camera2D
+
+
 var currentCardCount : int = 0
 var pickedUpCard : CardBase
 
@@ -10,3 +18,11 @@ func ReturnEmptySlot():
 		if slot.get_child_count() == 0:
 			return slot
 	return null
+	
+func _input(event):
+	if event is InputEventMouseMotion and Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		position -= event.relative * dragSensitivity / camNode.zoom 
+
+
+
+
